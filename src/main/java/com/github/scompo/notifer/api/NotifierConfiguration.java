16087fcb608782bc4d4bfee1da77aab34b9dfb07
Mauro Scomparin <scompo@gmail.com>
2016-02-03 21:30:58 +0100

    added comments on NotifierConfiguration

--- src/main/java/com/github/scompo/notifer/api/NotifierConfiguration.java
+++ src/main/java/com/github/scompo/notifer/api/NotifierConfiguration.java
@@ -2,8 +2,14 @@ package com.github.scompo.notifer.api;
 
 import com.github.scompo.utils.startable.Startable;
 
+/**
+ * Notifier configuration definition.
+ */
 public interface NotifierConfiguration extends Startable{
 
+	/**
+	 * Should implement the configuration of the notifiers. 
+	 */
 	void doConfiguration();
 
 }
